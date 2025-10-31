f7b59c77af3ab88c0b7d57ac15652efdbd0528e8
Dipesh Shrestha <dipeshsth00@gmail.com>
2025-10-31 12:33:38 +0545

    the signup form component is create

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,27 @@
+"use client";
+
+import SignUp from "@/components/auth/SignUp";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
+  const handlClick = () => {
+    alert("button clicked");
+  };
   return (
     <div>
-      <Button variant={"destructive"}>Click me</Button>
-      <Button variant={"outline"}>Click Aj</Button>
-      <Button variant={"ghost"}>Click Sushant</Button>
+      <Button onClick={handlClick} variant={"destructive"}>
+        Click me
+      </Button>
+      <Button onClick={handlClick} variant={"outline"}>
+        Click Aj
+      </Button>
+      <Button onClick={handlClick} variant={"ghost"}>
+        Click Sushant
+      </Button>
+
+      <div className="">
+        <SignUp />
+      </div>
     </div>
   );
 }
