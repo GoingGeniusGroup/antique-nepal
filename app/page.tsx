090af12060c2a93d8f30f0281e71fda34f8c534e
ajaysth <shrestha11ajay@gmail.com>
2025-11-01 08:33:30 +0545

    product showcase section added

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
 import { FeaturedCollection } from "@/components/featured-collection";
 
 import { Hero } from "@/components/hero";
+import ProductShowcase from "@/components/ProductShowcase";
 
 import { WhyChoose } from "@/components/why-choose";
 
 export default function Home() {
   return (
     <div>
-      <Hero/>
+      <Hero />
       <WhyChoose />
-      <FeaturedCollection/>
+      <FeaturedCollection />
+      <ProductShowcase />
     </div>
   );
 }
