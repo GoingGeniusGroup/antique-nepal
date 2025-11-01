7da26d4bf95201bd367d4ca93caa14cba45365f9
Dipesh Shrestha <dipeshsth00@gmail.com>
2025-11-01 10:58:47 +0545

    merge conflict resolve

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,32 +1,6 @@
-<<<<<<< HEAD
-"use client";
-
 import * as React from "react";
+
 import { cn } from "@/lib/utils";
-
-const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
-  ({ className, type = "text", ...props }, ref) => {
-    return (
-      <input
-        type={type}
-        className={cn(
-          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-base font-inter ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-          className
-        )}
-        ref={ref}
-        {...props}
-      />
-    );
-  }
-);
-
-Input.displayName = "Input";
-
-export { Input };
-=======
-import * as React from "react"
-
-import { cn } from "@/lib/utils"
 
 function Input({ className, type, ...props }: React.ComponentProps<"input">) {
   return (
@@ -41,8 +15,7 @@ function Input({ className, type, ...props }: React.ComponentProps<"input">) {
       )}
       {...props}
     />
-  )
+  );
 }
 
-export { Input }
->>>>>>> authentication
+export { Input };
